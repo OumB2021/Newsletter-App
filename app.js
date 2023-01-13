d7ac7ff1a2f2f9f019c599693250ac6abc365b9d
Oumar <baarryoumar@gmail.com>
2023-01-13 18:10:05 -0500

    body parser and post commands

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-//jshint esversion: 6
-
 const express = require('express');
-const request = require('reqyest');
+const request = require('request');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
 
+app.post("/", function(req, res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+});
 
 app.listen(3000, function(){
   console.log('listening on 3000');
