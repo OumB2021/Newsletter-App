99a202f685bc7b7338b15918e422af2e514396cb
Oumar <baarryoumar@gmail.com>
2023-01-13 19:31:53 -0500

    mailchimp list id

--- app.js
+++ app.js
@@ -12,12 +12,34 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [{
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+        }
+    }]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  const url = 
+
+  https.request(url, options, function(response){
+
+  });
 
 });
 
 app.listen(3000, function(){
   console.log('listening on 3000');
 });
+
+//d14473a575136c7aebeba86ac5d46607-us8
+//e4b700f286
